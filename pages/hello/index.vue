140e95935b0fc0b9040e6cfb0f11efcb147c1d8d
Ersan Karimi <ersankarimii311@gmail.com>
2025-03-18 00:01:05 +0800

    feat: add hello world page with Example component and meta tags

---
+++ pages/hello/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import Example from "./partials/Example.vue";
+
+useHead({
+  title: "Hello World!",
+  meta: [
+    {
+      name: "description",
+      content: "This is a hello world page",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <h1>
+      Hello World!
+    </h1>
+    <Example />
+    <NuxtLink to="/">
+      Go back home
+    </NuxtLink>
+  </div>
+</template>
